fd727e974625626b812aa122851fe8e66ba561f6
j540 <james@tokyowheel.com>
2014-10-26 21:56:43 +0700

    change deface text

--- app/overrides/add_hello_world_below_body_tag.rb
+++ app/overrides/add_hello_world_below_body_tag.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
   :name => 'add_hello_world_below_body_tag',
   :insert_top =>  "[data-hook='body']",
-  :text => '<h1>Hello World</h1>')
+  :text => '<h1>Hello Earth</h1>')
